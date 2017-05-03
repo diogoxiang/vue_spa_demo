342a9f543d981377628ccfc7a14be1191b37f9a0
a233894432 <a233894432@gmail.com>
2017-05-03 16:04:04 +0800

    学习调试

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,14 +10,14 @@ const rootPath = '';
 
 // 页面路由
 const routes = [
-  {path: '', redirect: {name: 'hello'}},
-  {path: '/hello', component: Hello, name: 'hello'}
+    { path: '', redirect: { name: 'hello' } },
+    { path: '/hello', component: Hello, name: 'hello' }
 ].map(route => {
-  route.path = rootPath + route.path;
-  return route;
+    route.path = rootPath + route.path;
+    return route;
 });
 
 // 404 页
-routes.push({path: '*', component: NotFound, name: 'notfound'});
+routes.push({ path: '*', component: NotFound, name: 'notfound' });
 
 export default routes;
--- src/views/hello.vue
+++ src/views/hello.vue
@@ -26,7 +26,7 @@
       }
     },
     mounted () {
-      this.$store.commit('message', '欢迎使用 vue！');
+      this.$store.commit('message', '欢迎使用');
       this.getContent();
     },
 
